7677866f062505a4eda7d3c5ebc76e47ecf84504
taujla <taujla@uoguelph.ca>
2026-05-14 14:30:57 -0400

    Initial commit

---
+++ portscanner.py
@@ -0,0 +1,22 @@
+import socket
+
+# use sockets to connect to certain IP/server at a certain port
+# if connection is made its open and vise vera
+
+target = "127.0.0.1" # local host IP
+
+def portscan(port):
+    try: 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+
+    except: 
+        return False
+
+for port in range (1, 1024):
+    result = portscan(port)
+    if result:
+        print("Port {} is open!".format(port))
+    else: 
+        print("Port {} is closed!".format(port)) 
